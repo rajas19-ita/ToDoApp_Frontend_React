c9fa06e19ef90ed62c33a5b842528a12a5b36b8a
rajas19-ita <rajaskadge@gmail.com>
2024-02-09 12:12:36 +0530

    heading font size changed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     return (
         <div className="bg-[#3a3a3a] h-screen w-screen text-white overflow-y-auto">
             <div className=" max-w-3xl mx-auto flex flex-col items-center gap-6 py-6 h-full">
-                <h1 className="text-3xl font-bold">My Todos</h1>
+                <h1 className="text-4xl font-bold">My Todos</h1>
                 <ToDoForm />
                 <ToDoList />
             </div>
